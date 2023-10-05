1248721f4a1ee41a235c399daaaedfce6ef1cf24
akrama23 <arundelfootball23@gmail.com>
2023-10-04 23:32:13 -0400

    adding react router and adding routes

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+
+import NavBar from './components/NavBar';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Portfolio from './pages/portfolio/Portfolio';
+
+
 
 function App() {
   
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='portfolio' element={<Portfolio />} />
+        <Route path='contact' element={<Contact />} />
+
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
 
